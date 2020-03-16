4c19f71d8db1f6b3263cb29eeaaca478fa030f52
unknown <mantenimientoyprevencion19@gmail.com>
2020-03-16 16:26:48 -0500

    asd

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 // conectar base de datos cleiser
 connectDB();
 // middleware bodyparser
@@ -9,6 +10,7 @@ app.use(bodyparser.urlencoded({
     extended: true
 }));
 app.use(bodyparser.json());
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 app.get('', (req, res) => {
     res.send(`
