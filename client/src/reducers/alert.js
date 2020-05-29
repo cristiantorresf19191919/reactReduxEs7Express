7566c0c06c136d05cd772c17026df5ab4f47ebc4
cristian <cristian.torres19@hotmail.com>
2020-05-29 19:10:06 -0400

    r branch

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -5,6 +5,7 @@ import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 const initialState = [];
 
 export default function (state = initialState, action){
+    // test
 
     switch (action.type) {
         case SET_ALERT:
